6b3071298e410d5928bde25e0fd9134c15cacdd2
9k3pthhurh8f9z <115055404+2ufkpfb9daxnik@users.noreply.github.com>
2025-01-09 05:37:21 +0900

    5まで完了

---
+++ src/app/api/admin/posts/[id]/route.ts
@@ -0,0 +1,130 @@
+import prisma from "@/lib/prisma";
+import { NextResponse, NextRequest } from "next/server";
+import { Post } from "@prisma/client";
+import { error } from "console";
+
+//単一記事の取得
+export const GET = async (
+  req: NextRequest,
+  { params: { id } }: { params: { id: string } }
+) => {
+  try {
+    const post = await prisma.post.findUnique({
+      where: { id },
+      select: {
+        id: true,
+        title: true,
+        content: true,
+        createdAt: true,
+        categories: {
+          select: {
+            category: {
+              select: {
+                id: true,
+                name: true,
+              },
+            },
+          },
+        },
+      },
+    });
+
+    if (!post) {
+      return NextResponse.json(
+        { error: `id='${id}'の投稿記事は見つかりませんでした` },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json(post, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "記事の取得に失敗しました" },
+      { status: 404 }
+    );
+  }
+};
+
+//単一記事の削除
+export const DELETE = async (
+  req: NextRequest,
+  { params: { id } }: { params: { id: string } }
+) => {
+  try {
+    const post: Post = await prisma.post.delete({ where: { id } });
+    return NextResponse.json({ msg: `「${post.title}」を削除しました。` });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "投稿記事の削除に失敗しました" },
+      { status: 500 }
+    );
+  }
+};
+
+//投稿記事の更新
+type RequestBody = {
+  title: string;
+  content: string;
+  coverImageUrl: string;
+  categoryIds: string[];
+};
+
+export const PUT = async (
+  req: NextRequest,
+  { params: { id } }: { params: { id: string } }
+) => {
+  try {
+    const { title, content, coverImageUrl, categoryIds }: RequestBody =
+      await req.json();
+
+    //カテゴリIDの検証
+    const validCategories = await prisma.category.findMany({
+      where: {
+        id: {
+          in: categoryIds,
+        },
+      },
+    });
+
+    if (validCategories.length !== categoryIds.length) {
+      return NextResponse.json(
+        { error: " 無効なカテゴリIDが含まれています" },
+        { status: 400 }
+      );
+    }
+
+    //トランザクションを使用してデータの整合性を保つ
+    const updatedPost = await prisma.$transaction(async (prisma) => {
+      //中間テーブルから現在の紐づけ情報を削除
+      await prisma.postCategory.deleteMany({
+        where: { postId: id },
+      });
+
+      //投稿記事の内容を更新
+      const post = await prisma.post.update({
+        where: { id },
+        data: { title, content, coverImageURL: coverImageUrl },
+      });
+
+      //中間テーブルに新しい紐づけ情報を追加
+      await prisma.postCategory.createMany({
+        data: categoryIds.map((categoryId) => ({
+          postId: id,
+          categoryId,
+        })),
+      });
+
+      return post;
+    });
+
+    return NextResponse.json(updatedPost, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "カテゴリの名前変更に失敗しました" },
+      { status: 500 }
+    );
+  }
+};
